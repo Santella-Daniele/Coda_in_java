ef32c6867a5b4d2ec58652b964e8334cc402b352
Santella-Daniele <55455877+Santella-Daniele@users.noreply.github.com>
2020-04-28 12:20:55 +0200

    Add files via upload

---
+++ Coda.java
@@ -0,0 +1,65 @@
+class Coda {
+    private Nodo testa;
+    private Nodo coda;
+    private int cont;
+
+    public Coda() {
+        this.cont = 0;
+        this.testa = null;
+        this.coda = null;
+    }
+
+    public boolean verifica() {
+        return this.cont == 0;
+    }
+
+    public int size() {
+        return this.cont;
+    }
+
+    public void stampa()
+    {
+        Nodo c = this.testa;
+
+        System.out.printf("[%d] ", this.size());
+
+        while (c != null) {
+            System.out.printf("%d ---> ", c.valore);
+            c = c.next;
+        }
+        System.out.printf("NULL\n");
+    }
+
+    public void aggiungi(int valore)
+    {
+        Nodo c = new Nodo(valore);
+
+        this.cont++;
+
+        if (this.testa == null) {
+            this.testa = c;
+        } else {
+            this.coda.next = c;
+        }
+
+        this.coda = c;
+    }
+
+   
+    public int clear()
+    {
+        int tmp;
+
+        tmp = this.testa.valore;
+
+        this.testa = this.testa.next;
+
+        if (this.testa == null) {
+            this.coda = null;
+        }
+
+        this.cont--;
+
+        return tmp;
+    }
+};
---
+++ Main.java
@@ -0,0 +1,46 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		Coda c = new Coda();
+        int tmp;
+
+        System.out.printf("E' vuota ? %s\n", c.verifica() ? "Si" : "No");
+        c.stampa();
+        System.out.println();
+
+        
+        c.aggiungi(1);
+        
+         System.out.printf("E' vuota ? %s\n", c.verifica() ? "Si" : "No");
+        c.stampa();
+        System.out.println();
+
+        c.aggiungi(2);
+        
+        System.out.printf("E' vuota ? %s\n", c.verifica() ? "Si" : "No");
+        c.stampa();
+        System.out.println();
+        
+        c.aggiungi(3);
+        
+        System.out.printf("E' vuota ? %s\n", c.verifica() ? "Si" : "No");
+        c.stampa();
+        System.out.println();
+
+        
+        if (!c.verifica()) {
+            tmp = c.clear();
+            System.out.printf("Elemento: %d\n", tmp);
+        }
+     
+       System.out.printf("E' vuota ? %s\n", c.verifica() ? "Yes" : "No");
+        c.stampa();
+        System.out.println();
+
+       
+    
+
+	}
+}
